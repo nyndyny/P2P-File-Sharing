f3f425070d4a870c45ec0abc20a7f8d3da82fd7b
Nandini Tripathi <73672777+nyndyny@users.noreply.github.com>
2023-10-24 02:46:56 -0400

    Update Unchoke.java

--- src/messageTypes/Unchoke.java
+++ src/messageTypes/Unchoke.java
@@ -1,1 +1,20 @@
+package messageTypes;
 
+import java.nio.ByteBuffer;
+
+public class Unchoke
+{
+	public byte[] unchokeMsg = new byte[5];
+	public byte[] msgLen = new byte[4];
+	
+	public Unchoke()
+	{
+		msgLen = ByteBuffer.allocate(4).putInt(0).array(); 
+		int i;
+		for(i = 0; i < messageLen.length; i++)
+		{
+			unchokeMsg[i] = msgLen[i];
+		}
+		unchokeMsg[i] = 1; //1 -> unchoke message type
+	}
+}
