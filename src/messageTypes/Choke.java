f8e5db1b49e12739eab935fba5fd0a5ab6e5679d
Nandini Tripathi <73672777+nyndyny@users.noreply.github.com>
2023-10-24 02:42:48 -0400

    Update Choke.java

--- src/messageTypes/Choke.java
+++ src/messageTypes/Choke.java
@@ -1,1 +1,20 @@
+package messageTypes;
 
+import java.nio.ByteBuffer;
+
+public class Choke
+{
+    public byte[] chokeMessage = new byte[5];
+    public byte[] msgLength = new byte[4];
+
+    public Choke()
+    {
+        msgLength = ByteBuffer.allocate(4).putInt(0).array();
+        int i;
+        for(i = 0; i < msgLength.length; i++)
+        {
+            chokeMessage[i] = msgLength[i];
+        }
+        chokeMessage[i] = 0; //0 -> choke message type
+    }
+}
