b656c253245e89a5828564e4e517fb827ef7688e
Prajuna Venkatesan <venkatesanp@ufl.edu>
2023-10-24 20:12:19 -0400

    edit message files

--- src/Handshake.java
+++ src/Handshake.java
@@ -1,3 +1,11 @@
+/*
+    specified by Page 1 of Project Description
+    The handshake consists of three parts: handshake header, zero bits, and peer ID. The 
+    length of the handshake message is 32 bytes. The handshake header is 18-byte string 
+    ‘P2PFILESHARINGPROJ’, which is followed by 10-byte zero bits, which is followed by 
+    4-byte peer ID which is the integer representation of the peer ID. 
+*/
+
 import java.nio.charset.StandardCharsets;
 
 public class Handshake {
