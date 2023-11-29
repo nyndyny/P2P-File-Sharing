65515a0a77446dcc47400549dd9f1d6ca9b26be9
Nandini Tripathi <73672777+nyndyny@users.noreply.github.com>
2023-11-29 16:18:52 +0530

    Fixing Logs.java

--- src/Logs.java
+++ src/Logs.java
@@ -1,6 +1,5 @@
 // This file writes the peer log into the log file ‘log_peer_[peerID].log'
 
-import PeerInfo;
 import java.time.LocalDateTime;
 import java.io.File;
 import java.io.FileWriter;
@@ -12,68 +11,56 @@ import java.io.IOException;
 import java.io.FileOutputStream;
 
 public class Logs {
-    public static int peerID;
+    public static String peerID;
+    public static String peerAddress;
+    public static String peerPort;
+    public static String fileName;
     public static LocalDateTime localDT = LocalDateTime.now();
     public static File file;
     public static DateTimeFormatter formatDT = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
     private static BufferedWriter buffWriter;
-    PeerInfo peerInfo = new PeerInfo();
+    PeerInfo peerInfo = new PeerInfo(peerID, peerAddress, peerPort, fileName);
 
-    public static void startLogging(int pID)
-    {
+    public static void startLogging(String pID) {
         peerID = pID;
-        String name = (new File(System.getProperty("user.dir")).getParent() + "/log_peer_" + thisPeerID + ".log");
+        String name = (new File(System.getProperty("user.dir")).getParent() + "/log_peer_" + peerID + ".log");
         file = new File(name);
-        try 
-        {
+        try {
             buffWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
-        } 
-        catch (FileNotFoundException e) 
-        {
+        } catch (FileNotFoundException e) {
             System.err.println(e);
         }
     }
 
-    public static void choking(int pID)
-    {
-        try 
-        {
-            string temp = formatDT.format(localDT) + thisPeerID + " is choked by " + pID + ".";
+    public static void choking(int pID) {
+        try {
+            String temp = formatDT.format(localDT) + peerID + " is choked by " + pID + ".";
             buffWriter.append(temp);
             buffWriter.newLine();
             buffWriter.flush();
-        } 
-        catch(IOException e) 
-        {
+        } catch (IOException e) {
             System.err.println(e);
         }
     }
 
-    public static void unchoking(int pID)
-    {
-        try
-        {
-            String temp = formatDT.format(localDT) + " : Peer " + thisPeerID + " is unchoked by " + pID + ".";
+    public static void unchoking(int pID) {
+        try {
+            String temp = formatDT.format(localDT) + " : Peer " + peerID + " is unchoked by " + pID + ".";
             buffWriter.append(temp);
             buffWriter.newLine();
             buffWriter.flush();
-        }
-        catch(IOException e)
-        {
+        } catch (IOException e) {
             System.err.println(e);
         }
     }
 
-    //TODO: add logs for other scenarios like TCP connection, downloading a piece, receiving messages, etc.
+    // TODO: add logs for other scenarios like TCP connection, downloading a piece,
+    // receiving messages, etc.
 
-    public static void endLogging()
-    {
-        try 
-        {
+    public static void endLogging() {
+        try {
             buffWriter.close();
-        } 
-        catch (IOException e) 
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
